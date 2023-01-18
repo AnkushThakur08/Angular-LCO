247526d244a996ed614fe91551c47cce0e3efd15
Ankush Thakur <atpara99@gmail.com>
2023-01-18 18:30:55 +0530

    More Coded Added

--- five-todo-app/src/app/app.module.ts
+++ five-todo-app/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Components
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { HeaderComponent } from './layout/header/header.component';
+
+// FontAwesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+// Form
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoFormComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FontAwesomeModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- five-todo-app/src/app/components/todo-form/todo-form.component.ts
+++ five-todo-app/src/app/components/todo-form/todo-form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TodoService } from 'src/app/service/todo.service';
+
+import { Todo } from '../../model/Todo';
+
+// import { v4 } from 'uuid';
+
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
-  styleUrls: ['./todo-form.component.css']
+  styleUrls: ['./todo-form.component.css'],
 })
 export class TodoFormComponent implements OnInit {
+  constructor(private todoService: TodoService) {}
 
-  constructor() { }
+  todoTitle: string = '';
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  addTodo = () => {
+    const newTodo = {
+      id: '1',
+      title: this.todoTitle,
+      date: new Date(),
+    };
+  };
 }
