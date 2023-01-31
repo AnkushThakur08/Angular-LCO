4a43fa2dac372f702f7f9e3c8916d205e5c8472a
Ankush Thakur <atpara99@gmail.com>
2023-01-31 12:11:29 +0530

    Project Redesigned Again

--- nine-socialmedia-app/karma.conf.js
+++ nine-socialmedia-app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/nine-socialmedia-app'),
+      dir: require('path').join(__dirname, './coverage/nine-socialMedia-App'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- nine-socialmedia-app/src/app/app.component.ts
+++ nine-socialmedia-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nine-socialmedia-app';
+  title = 'nine-socialMedia-App';
 }
