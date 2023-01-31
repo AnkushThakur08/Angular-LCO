39bdf193b3c8c912aa11b1c29ae371fbdfd569ec
Ankush Thakur <atpara99@gmail.com>
2023-01-31 17:13:16 +0530

    Social Media App Header Footer Completed

--- ten-social-Media-App/src/app/layout/header/header.component.ts
+++ ten-social-Media-App/src/app/layout/header/header.component.ts
@@ -15,11 +15,30 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  email!: any;
   constructor(
     private auth: AuthService,
     private toastr: ToastrService,
     private router: Router
-  ) {}
+  ) {
+    /* As soon as Header Comes up, I want to check, I want to display the email of the user */
+    auth.getUser().subscribe((user) => {
+      this.email = user?.email;
+
+      console.log('USER OBJ', user);
+    });
+  }
 
   ngOnInit(): void {}
+
+  async handleSignOut() {
+    try {
+      await this.auth.signOut();
+      this.toastr.success('Signout Successfully');
+      this.router.navigateByUrl('/signin');
+      this.email = '';
+    } catch (error) {
+      this.toastr.error('Problem in signOut');
+    }
+  }
 }
